1b3a4cb1a0eaf77973194b663bae53614fe920d8
Facundo Gabriel Schillino Gariglio <111167970+facundoschillino@users.noreply.github.com>
2025-08-20 18:06:16 -0300

    Update admin.py

--- prueba/admin.py
+++ prueba/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 from .models import Foto
 
-
+@admin.register(Foto)
 class FotoAdmin(admin.ModelAdmin):
     list_display = ("id", "titulo", "preview", "creado")
     search_fields = ("titulo",)
